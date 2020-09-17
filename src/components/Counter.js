c0b20194d5dd5e4c6d45b95ef173dff26beafb3b
ORiveraJr84 <oriverajr84@gmail.com>
2020-09-16 22:20:13 -0400

    Step 2 moods.js done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,30 +46,30 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React, { useState } from "react" /* STEP 0 */
+import React, { useState } from "react"; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     /* STEP 4 */
     setCount(count + 1);
-  }
+  };
   const decrement = () => {
     /* STEP 5 */
     setCount(count - 1);
-  }
+  };
   const reset = () => {
     /* STEP 6 */
     setCount(0);
-  }
+  };
 
   const style = {
     fontSize: "1.5em",
     marginBottom: "0.3em",
     color: count % 2 == 0 ? "royalblue" : "crimson" /* STEP 2 */,
-  }
+  };
 
   return (
     <div className="widget-counter container">
@@ -89,5 +89,5 @@ export default function Counter() {
         </button>
       </div>
     </div>
-  )
+  );
 }

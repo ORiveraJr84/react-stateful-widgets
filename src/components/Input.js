72395814f4ab25f15ac7a14b589f7259127860dc
ORiveraJr84 <oriverajr84@gmail.com>
2020-09-16 23:23:38 -0400

    Step 2 Input.js updated

--- src/components/Input.js
+++ src/components/Input.js
@@ -54,13 +54,16 @@ export default function Input() {
   const style = {
     fontSize: "1.5em",
     marginBottom: "0.3em",
-    color: inputValue.length < 10 ? "royalblue" : "crimson" /* STEP 2 */,
+    color: inputValue.length > 10 ? "crimson" : "royalblue" /* STEP 2 */,
   };
 
   return (
     <div className="widget-input container">
       <h2>Input</h2>
-      <div id="output" style={style}></div> {/* STEP 3 */}
+      <div id="output" style={style}>
+        {inputValue.toUpperCase()}
+      </div>{" "}
+      {/* STEP 3 */}
       <div>
         <input id="input" type="text" onChange={changeInput} /> {/* STEP 6 */}
         <button id="resetInput" onClick={reset}>
